5707a0bf7fcd8072b100d59ca316512b72568abd
Arjan Scherpenisse <arjan@scherpenisse.net>
2009-12-29 21:07:09 +0100

    Added more whitespace

--- twisted/protocols/osc.py
+++ twisted/protocols/osc.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 """
 OSC 1.1 Protocol over UDP for Twisted.
-http://opensoundcontrol.org/spec-1_1 
+http://opensoundcontrol.org/spec-1_1
 """
+
 class OscError(Exception):
     """
     Any error raised by this module.
     """
     pass
 
+
 class Message(object):
     """
     OSC Message
     """
+
     def __init__(self, address, type_tags=None, arguments=None):
         self.address = address
         self.type_tags = type_tags
         self.arguments = arguments
+
 
 class Bundle(object):
     """
@@ -27,6 +31,8 @@ class Bundle(object):
         self.time_tag = time_tag
         if self.time_tag is None:
             #TODO create time tag
+            pass
+
 
 class Argument(object):
     """
@@ -35,43 +41,58 @@ class Argument(object):
     def __init__(self, value):
         self.value = value
 
+
 class BlobArgument(Argument):
     pass
+
+
 class StringArgument(Argument):
     pass
+
+
 class IntArgument(Argument):
     pass
+
+
 class LongArgument(Argument):
     pass
+
+
 class FloatArgument(Argument):
     pass
+
+
 class TimeTagArgument(Argument):
     pass
+
+
 class DoubleArgument(Argument):
     pass
-class SymbolArgument(Argument): 
+
+
+class SymbolArgument(Argument):
     pass
     #FIXME: what is that?
 
 _types = {
-    float: FloatArgument, 
-    str: StringArgument, 
+    float: FloatArgument,
+    str: StringArgument,
     int: IntArgument, # FIXME: or long?
-    unicode: StringArgument, 
+    unicode: StringArgument,
     #TODO : more types
     }
 _tags = {
-    "f": FloatArgument, 
-    "s": StringArgument, 
-    "i": IntArgument, 
+    "f": FloatArgument,
+    "s": StringArgument,
+    "i": IntArgument,
     #TODO : more types
     }
-    
+
 
 def createArgument(data, type_tag=None):
     """
     Creates an OSC argument, trying to guess its type if no type is given.
-    
+
     Factory of *Attribute object.
     :param data: Any Python base type.
     :param type_tag: One-letter string. Either "i", "f", etc.
