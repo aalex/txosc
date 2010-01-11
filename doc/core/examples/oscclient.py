6c297650ae71f40d1562a0bafa31e25a9a2aa240
Alexandre Quessy <alexandre@quessy.net>
2010-01-11 02:33:14 -0500

    Improved examples by adding sub-nodes and arguments.

--- doc/core/examples/oscclient.py
+++ doc/core/examples/oscclient.py
@@ -10,13 +10,18 @@ if __name__ == "__main__":
     ds = osc.Sender()
     port = 17777
     def send_messages(ds, port):
+        # no argument
         ds.send(osc.Message("/ping"), ("127.0.0.1", port))
         # float argument
-        ds.send(osc.Message("/ham", 3.14159), ("127.0.0.1", port))
+        ds.send(osc.Message("/ham/egg", 3.14159), ("127.0.0.1", port))
         # str and int arguments
         ds.send(osc.Message("/spam", "hello", 1), ("127.0.0.1", port))
         # NTP timestamp argument. See http://opensoundcontrol.org/spec-1_0
         ds.send(osc.Message("/bacon", osc.TimeTagArgument()), ("127.0.0.1", port))
+        # no argument
+        ds.send(osc.Message("/cheese"), ("127.0.0.1", port))
+        ds.send(osc.Message("/cheese/cheddar"), ("127.0.0.1", port))
+
         reactor.stop()
 
     reactor.callLater(0, send_messages, ds, port)
--- doc/core/examples/oscserver.py
+++ doc/core/examples/oscserver.py
@@ -8,14 +8,37 @@ from twisted.protocols import osc
 
 if __name__ == "__main__":
     receiver = osc.Receiver()
-
+    
+    # Adding a simple callback for /ping:
     def ping(msg, addr):
-        print "PING!"
-        print msg, addr
+        print "PING!", msg, addr
     receiver.addCallback("/ping", ping)
+    
+    # Adding a callback for /ham/egg:
+    def ham_egg(msg, addr):
+        print "Ham and egg", msg, addr
+    receiver.addCallback("/ham/egg", ham_egg)
+
+    # Adding a callback for any message:
+    # (note that it is called on every incoming message)
     def prnt(msg, addr):
-        print "Message from", addr, ":", msg
+        print "Catch-all: ", addr, ":", msg
     receiver.addCallback("/*", prnt)
 
+    # Adding a node for /cheese:
+    def cheese(msg, addr):
+        print "Got cheese from", addr, ":", msg
+    cheeseNode = osc.AddressNode("cheese")
+    cheeseNode.addCallback("*", cheese) # not only leaves can have callbacks in this implementation of OSC
+    receiver.addNode("cheese", cheeseNode)
+
+    # Adding a node for /cheese/cheddar:
+    def cheddar(msg, addr):
+        print "Got cheddar from", addr, ":", msg
+    cheddarNode = osc.AddressNode("cheddar")
+    cheddarNode.addCallback("*", cheddar)
+    cheeseNode.addNode("cheddar", cheddarNode)
+    
+    # Starting the server:
     reactor.listenUDP(17777, receiver.getProtocol())
     reactor.run()
