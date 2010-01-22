70a69efa50899e895c21d377d13176abd5cc7c98
Arjan Scherpenisse <arjan@scherpenisse.net>
2010-01-22 11:39:55 +0100

    Added test with liblo sender.

--- twisted/test/test_osc.py
+++ twisted/test/test_osc.py
@@ -8,8 +8,9 @@ Maintainer: Arjan Scherpenisse
 """
 
 from twisted.trial import unittest
-from twisted.internet import reactor, defer
+from twisted.internet import reactor, defer, task
 from twisted.protocols import osc
+
 
 class TestArgumentCreation(unittest.TestCase):
     """
@@ -632,23 +633,22 @@ class ClientServerTests(object):
         bundle = osc.Bundle()
         bundle.add(osc.Message("/pong"))
         bundle.add(pingMsg)
-        bundle.add(osc.Message("/foo/bar", 1, 2, 3.13))
+        bundle.add(osc.Message("/foo/bar", 1, 2))
 
         d = defer.Deferred()
         def ping(m, addr):
             self.assertEquals(m, pingMsg)
             d.callback(True)
 
-        # d2 = defer.Deferred()
-        # def foo(m, addr):
-        #     self.assertEquals(m, osc.Message("/foo/bar", 1, 2, 3.13))
-        #     d2.callback(True)
+        d2 = defer.Deferred()
+        def foo(m, addr):
+            self.assertEquals(m, osc.Message("/foo/bar", 1, 2))
+            d2.callback(True)
 
         self.receiver.addCallback("/ping", ping)
-        #self.receiver.addCallback("/foo/*", foo)
+        self.receiver.addCallback("/foo/*", foo)
         self._send(bundle)
-        return d
-        #return defer.DeferredList([d, d2])
+        return defer.DeferredList([d, d2])
 
 
 
@@ -656,6 +656,7 @@ class TestUDPClientServer(unittest.TestCase, ClientServerTests):
     """
     Test the L{osc.Sender} and L{osc.Receiver} over UDP via localhost.
     """
+    timeout = 1
 
     def setUp(self):
         self.receiver = osc.Receiver()
@@ -677,6 +678,7 @@ class TestTCPClientServer(unittest.TestCase, ClientServerTests):
     """
     Test the L{osc.Sender} and L{osc.Receiver} over UDP via localhost.
     """
+    timeout = 1
 
     def setUp(self):
         self.receiver = osc.Receiver()
@@ -700,12 +702,86 @@ class TestReceiverWithExternalClient(unittest.TestCase):
     """
     This test needs python-liblo.
     """
-    pass
+    timeout = 1
+
+    def setUp(self):
+        self.receiver = osc.Receiver()
+        self.serverPort = reactor.listenUDP(17778, osc.DatagramServerProtocol(self.receiver))
+        self.target = liblo.Address(17778)
+
+    def tearDown(self):
+        return defer.DeferredList([self.serverPort.stopListening()])
+
+    def testSingleMessage(self):
+
+        d = defer.Deferred()
+        def ping(m, addr):
+            self.assertEquals(m, osc.Message("/ping"))
+            d.callback(True)
+
+        self.receiver.addCallback("/ping", ping)
+        liblo.send(self.target, "/ping")
+        return d
+
+
+    def testBundle(self):
+
+        d = defer.Deferred()
+        d2 = defer.Deferred()
+        def ping(m, addr):
+            self.assertEquals(m, osc.Message("/ping"))
+            d.callback(True)
+        def pong(m, addr):
+            self.assertEquals(m, osc.Message("/pong", 1, 2, "string"))
+            d2.callback(True)
+
+        self.receiver.addCallback("/ping", ping)
+        self.receiver.addCallback("/po*", pong)
+
+        b = liblo.Bundle()
+        b.add("/ping")
+        b.add("/pong", 1, 2, "string")
+        liblo.send(self.target, b)
+        return defer.DeferredList([d, d2])
+
 
 
 class TestClientWithExternalReceiver(unittest.TestCase):
     """
     This test needs python-liblo.
     """
-    pass
+    timeout = 1
 
+    def setUp(self):
+        self.client = osc.DatagramClientProtocol()
+        self.clientPort = reactor.listenUDP(0, self.client)
+
+
+    def tearDown(self):
+        return defer.DeferredList([self.clientPort.stopListening()])
+
+
+    def _send(self, element):
+        self.client.send(element, ("127.0.0.1", 17778))
+
+    def testSingleMessage(self):
+        server = liblo.Server(17779)
+        server.start()
+
+        received = False
+        def ping_callback(path, args):
+            received = True
+        server.add_method("/ping", '', ping_callback)
+
+        self._send(osc.Message("/ping"))
+
+        while not received:
+            print 11
+            server.recv(100)
+
+
+try:
+    import liblo
+except ImportError:
+    TestReceiverWithExternalClient.skip = "pyliblo not installed"
+TestClientWithExternalReceiver.skip = "FIXME: liblo server does not run with twisted"
