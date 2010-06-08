ee05d52f1df92ba66826ec82525a073e2714d520
Alexandre Quessy <alexandre@quessy.net>
2010-06-08 10:16:27 -0400

    updated project URL and added mailing list

--- setup.py
+++ setup.py
@@ -11,11 +11,16 @@ setup(
     name = "txosc",
     version = __version__,
     author = "Arjan Scherpenisse and Alexandre Quessy",
-    author_email = "alexandre@quessy.net",
-    url = "http://bitbucket.org/arjan/twisted-osc",
+    author_email = "txosc@toonloop.com",
+    url = "http://bitbucket.org/arjan/txosc",
     description = "Open Sound Control Protocol for Twisted",
-    install_requires = [], # twisted
+    #install_requires = ["twisted"],
     scripts = [],
-    packages = ["txosc", "txosc/test"]
+    license="MIT/X",
+    packages = ["txosc", "txosc/test"],
+    long_description = """Open Sound Control (OSC) is an open, transport-independent, message-based protocol developed for communication among computers, sound synthesizers, and other multimedia devices. 
+
+This library implements OSC version 1.1 over both UDP and TCP for the Twisted Python framework. 
+  """
     )
 
