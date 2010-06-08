c7d04fa6d5ffbfe1391d947109004562091d6e19
Alexandre Quessy <alexandre@quessy.net>
2010-06-08 10:27:28 -0400

    Re-adding oscclient.py and oscserver.py examples. I had forgotten them in the bug change from twisted fork to standalone txosc.

---
+++ examples/oscserver.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2009 Alexandre Quessy, Arjan Scherpenisse
+# See LICENSE for details.
+
+""" Simple OSC server. """
+
+from twisted.internet import reactor
+from twisted.protocols import osc
+
+if __name__ == "__main__":
+    receiver = osc.Receiver()
+    
+    # Adding a simple callback for /ping:
+    def ping(msg, addr):
+        print "PING! from %s" % repr(addr)
+    receiver.addCallback("/ping", ping)
+    
+    # Adding a callback for /ham/egg:
+    def ham_egg(msg, addr):
+        print "Ham and egg", msg, addr
+    receiver.addCallback("/ham/egg", ham_egg)
+
+    # Adding a callback for any message:
+    def fb(msg, addr):
+        print "Fallback: ", addr, ":", msg
+    receiver.fallback = fb
+
+    # Adding a node for /cheese:
+    def cheese(msg, addr):
+        print "Got cheese from", addr, ":", msg
+    cheeseNode = osc.AddressNode("cheese")
+    cheeseNode.addCallback("*", cheese) # not only leaves can have callbacks in this implementation of OSC
+    receiver.addNode("cheese", cheeseNode)
+
+    # Adding a node for /cheese/cheddar:
+    def cheddar(msg, addr):
+        print "Got cheddar from", addr, ":", msg
+
+    cheddarNode = osc.AddressNode("cheddar")
+    cheddarNode.addCallback("*", cheddar)
+    cheeseNode.addNode("cheddar", cheddarNode)
+
+    # Starting the server on UDP:
+    reactor.listenUDP(17777, osc.DatagramServerProtocol(receiver))
+
+    # Starting the server on TCP:
+    reactor.listenTCP(17776, osc.ServerFactory(receiver))
+    reactor.run()
