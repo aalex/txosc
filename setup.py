b0a2c934a8835d6682a3651565e7ada18581d3a8
Alexandre Quessy <alexandre@quessy.net>
2010-06-09 00:40:50 -0400

    updated version number to 0.1.1... not yet released it though

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ txosc installation script
 
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 setup(
     name = "txosc",
--- txosc/__init__.py
+++ txosc/__init__.py
@@ -3,3 +3,4 @@
 TxOSC: Open Sound Control for Twisted
 """
 __all__ = ["async", "dispatch", "osc"]
+__version__ = "0.1.1"
