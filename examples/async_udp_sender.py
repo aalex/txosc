edc7e1fb72424afd5a135db336ab804a2fe5162c
Alexandre Quessy <alexandre@quessy.net>
2010-06-09 00:31:04 -0400

    Merged the async_udp examples with the former oscclient and oscserver examples.

--- examples/async_udp_sender.py
+++ examples/async_udp_sender.py
@@ -21,15 +21,24 @@ class UDPSenderApplication(object):
         reactor.callLater(0, self.send_messages)
 
     def _send(self, element):
+        # This method is defined only to simplify the example
         self.client.send(element, (self.host, self.port))
         print("Sent %s to %s:%d" % (element, self.host, self.port))
         
     def send_messages(self):
         self._send(osc.Message("/ping"))
         self._send(osc.Message("/foo"))
+        self._send(osc.Message("/ham/egg"))#, 3.14159))
+        self._send(osc.Message("/spam", "hello", 1))
+        self._send(osc.Message("/bacon", osc.TimeTagArgument()))
+        self._send(osc.Message("/cheese"))
+        self._send(osc.Message("/cheese/cheddar"))
+        # of course, the /quit message has to be sent last!
         self._send(osc.Message("/quit"))
-        reactor.stop()
         print("Goodbye.")
+        def _stop():
+            reactor.stop()
+        reactor.callLater(0.1, _stop)
 
 if __name__ == "__main__":
     app = UDPSenderApplication(17779)
