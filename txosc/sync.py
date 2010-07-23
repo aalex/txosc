a4adf08440e1e057d3dbce6c71d17bc00e8f3e5b
Alexandre Quessy <alexandre@quessy.net>
2010-07-23 12:14:05 -0400

    Addin a non-wroking blocking UDP sender. (it doesn't work!!)

--- txosc/sync.py
+++ txosc/sync.py
@@ -23,18 +23,73 @@ class _Sender(object):
     def close(self):
         raise NotImplementedError("This method must be overriden in child classes.")
 
+#TODO: receiver and bidirectional sender-receiver
+# self._socket.recv(self.buffer_size)
+# self.buffer_size = 1024
+
 class TcpSender(_Sender):
+    """
+    Send OSC over TCP using low-level Python socket tools.
+    """
     def __init__(self, address, port):
         _Sender.__init__(self)
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address = address
         self.port = port
-        self.buffer_size = 1024
         self._socket.connect((self.address, self.port))
 
     def _actually_send(self, binary_data):
         self._socket.send(binary_data)
-        # self._socket.recv(self.buffer_size) #FIXME
+
+    def close(self):
+        self._socket.close()
+
+UDP_MODE_MULTICAST = "multicast"
+UDP_MODE_BROADCAST = "broadcast"
+
+class UdpSender(_Sender):
+    """
+    Uses UDP.
+
+    Mode can be either UDP_MODE_BROADCAST or UDP_MODE_MULTICAST or None.
+    If using UDP_MODE_MULTICAST, you must set the multicast_group.
+    
+    FIXME: Right now, the data it sends is badly formatted.
+    """
+    def __init__(self, address, port, mode=None, multicast_group=None):
+        """
+        @param multicast_group: IP of the multicast group.
+        @type multicast_group: C{str}
+        """
+        _Sender.__init__(self)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.address = address
+        self.port = port
+        self._socket.bind(('', 0))
+        self.mode = mode
+        self.multicast_group = None
+        if self.mode == UDP_MODE_MULTICAST:
+            if multicast_group is None:
+                raise RuntimeError("You must set the multicast group.")
+            self.multicast_group = multicast_group
+            ttl = struct.pack('b', 1)
+            self._socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+            # TODO
+        elif self.mode == UDP_MODE_BROADCAST:
+            if multicast_group is not None:
+                raise RuntimeError("Not using the multicast mode.")
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        else:
+            if multicast_group is not None:
+                raise RuntimeError("Not using the multicast mode.")
+
+    def _actually_send(self, binary_data):
+        if self.mode == UDP_MODE_BROADCAST:
+            self._socket.sendto(binary_data, ('<broadcast>', self.port))
+        elif self.mode == UDP_MODE_MULTICAST:
+            self._socket.sendto(binary_data, (self.multicast_group, self.port))
+        else:
+            self._socket.sendto(binary_data, ('', self.port))
 
     def close(self):
         self._socket.close()
