c6bd019083ed6e8ffe6de9d2a7ecb67422eac35b
Alexandre Quessy <alexandre@quessy.net>
2010-07-23 08:14:26 -0400

    catching socket errors in sync_tcp_sender

--- examples/sync_tcp_sender.py
+++ examples/sync_tcp_sender.py
@@ -4,12 +4,18 @@ Example of a TCP txosc sender without Twisted.
 
 This example is in the public domain.
 """
+import socket # only for the exception type
 from txosc import osc
 from txosc import sync
 
 if __name__ == "__main__":
-    tcp_sender = sync.TcpSender("localhost", 31337)
-    tcp_sender.send(osc.Message("/hello", 2, "bar", 3.14159))
-    tcp_sender.send(osc.Message("/ham/spam", "egg"))
-    tcp_sender.close()
+    try:
+        tcp_sender = sync.TcpSender("localhost", 31337)
+    except socket.error, e:
+        print(str(e))
+    else:
+        tcp_sender.send(osc.Message("/hello", 2, "bar", 3.14159))
+        tcp_sender.send(osc.Message("/ham/spam", "egg"))
+        tcp_sender.close()
+        print("Successfully sent the messages.")
 
